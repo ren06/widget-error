3ac93619e7562cfc59e74112387459dea309b9f7
Renaud Theuillon <rtheuillon@synvance.com>
2021-05-21 14:19:06 +0100

    update

--- error/error.js
+++ error/error.js
@@ -1,3 +1,80 @@
 (function () {
-   
+    let template = document.createElement("template");
+    template.innerHTML = `
+		<style>
+		:host {
+			display: block;
+		} 
+
+        .container {
+            max-width: 400px;
+            overflow: hidden;
+        }
+
+        img {
+            width: 100%;
+            object-fit: contain;
+        }
+
+		</style>
+		
+		<div class="container">
+            <img alt="Button image" title="Button image" src="https://aydenowl.github.io/literate-enigma/image_button/bookmark.png" />
+		</div>
+	`;
+
+    class Imagebutton extends HTMLElement {
+        constructor() {
+            super();
+            let shadowRoot = this.attachShadow({ mode: "open" });
+            shadowRoot.appendChild(template.content.cloneNode(true));
+
+            this.$style = shadowRoot.querySelector('style');
+            this.$div = shadowRoot.querySelector('div');
+
+            console.log(this.$selector, this.$errorExists);
+
+            this.addEventListener("click", event => {
+                var event = new Event("onClick");
+                this.dispatchEvent(event);
+            });
+
+            this._props = {};
+        }
+
+        render(imageUrl, tooltip) {
+            this.$style.innerHTML = ':host {display: block;} .container {max-width: 400px;overflow: hidden;} img {width: 100%;object-fit: contain;}';
+            this.$div.innerHTML = '<img alt="Button image" title="' + tooltip + '" src="' + imageUrl + '" />';
+        }
+
+        onCustomWidgetBeforeUpdate(changedProperties) {
+            this._props = { ...this._props, ...changedProperties };
+        }
+
+        onCustomWidgetAfterUpdate(changedProperties) {
+            if ("imageUrl" in changedProperties) {
+                this.$imageUrl = changedProperties["imageUrl"];
+            }
+
+            if ("tooltip" in changedProperties) {
+                this.$tooltip = changedProperties["tooltip"];
+            }
+
+            if ("selector" in changedProperties) {
+                this.$selector = changedProperties["selector"];
+                const errorMessage = document.querySelector(this.$selector);
+                if(errorMessage){
+                    this.$errorExists = true;
+                }
+            }
+
+            if ("errorExists" in changedProperties) {
+                this.$errorExists = changedProperties["errorExists"];
+            }
+
+            this.render(this.$imageUrl, this.$tooltip);
+        }
+    }
+
+    customElements.define("com-synvance-imagebutton", Imagebutton);
 })();
